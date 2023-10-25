dc51f95c4395afca7c985893fc3dbe77b5c6a2aa
kmsgm <minseo.kim@friendli.ai>
2023-10-25 20:35:17 +0900

    endpoint

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // export const API_ENDPOINT = 'http://127.0.0.1:8000';
-export const API_ENDPOINT = 'https://minseo-portfolio-4f6b9f454d74.herokuapp.com';
+// export const API_ENDPOINT = 'https://minseo-portfolio-4f6b9f454d74.herokuapp.com';
+export const API_ENDPOINT = 'http://3.39.23.121'
 
 const instance = axios.create({
   baseURL: API_ENDPOINT,
